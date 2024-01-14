166946b08c855eee5042733d5001a97d32c48330
Sav <davesavic27@gmail.com>
2024-01-14 10:17:47 +1000

    Added transition for mobile menu

--- view/layout_templ.go
+++ view/layout_templ.go
@@ -57,6 +57,33 @@ func AuthorisedLayout() templ.Component {
                         }, 75);
                     })
 
+                    const mobileMenu = document.querySelector('#mobile-menu')
+                    const mobileMenuButton = document.querySelector('#mobile-menu-button')
+                    const mobileMenuClosedIcon = document.querySelector('#mobile-menu-closed-icon')
+                    const mobileMenuOpenIcon = document.querySelector('#mobile-menu-open-icon')
+                    mobileMenuButton.addEventListener('click', () => {
+                        if (mobileMenu.classList.contains('hidden')) {
+                            // Show menu
+                            mobileMenu.classList.remove('hidden');
+
+                            mobileMenuOpenIcon.classList.remove('hidden');
+                            mobileMenuOpenIcon.classList.add('block');
+                            mobileMenuClosedIcon.classList.remove('block');
+                            mobileMenuClosedIcon.classList.add('hidden');
+                            return;
+                        }
+
+                        // Hide menu
+                        setTimeout(() => {
+                            mobileMenu.classList.add('hidden');
+
+                            mobileMenuOpenIcon.classList.remove('block');
+                            mobileMenuOpenIcon.classList.add('hidden');
+                            mobileMenuClosedIcon.classList.remove('hidden');
+                            mobileMenuClosedIcon.classList.add('block');
+                        }, 75);
+                    })
+
                     document.addEventListener('click', (event) => {
                         if (!menu.classList.contains('hidden') && !menuButton.contains(event.target)) {
                             // Hide menu
@@ -64,6 +91,18 @@ func AuthorisedLayout() templ.Component {
                             menu.classList.add('transition', 'ease-in', 'duration-75', 'opacity-0', 'scale-95');
                             setTimeout(() => {
                                 menu.classList.add('hidden');
+                            }, 75);
+                        }
+
+                        if (!mobileMenu.classList.contains('hidden') && !mobileMenuButton.contains(event.target)) {
+                            // Hide menu
+                            setTimeout(() => {
+                                mobileMenu.classList.add('hidden');
+
+                                mobileMenuOpenIcon.classList.remove('block');
+                                mobileMenuOpenIcon.classList.add('hidden');
+                                mobileMenuClosedIcon.classList.remove('hidden');
+                                mobileMenuClosedIcon.classList.add('block');
                             }, 75);
                         }
                     })
@@ -190,7 +229,7 @@ func AuthorisedLayout() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("--><button type=\"button\" class=\"relative inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2\" aria-controls=\"mobile-menu\" aria-expanded=\"false\"><span class=\"absolute -inset-0.5\"></span> <span class=\"sr-only\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("--><button id=\"mobile-menu-button\" type=\"button\" class=\"relative inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2\" aria-controls=\"mobile-menu\" aria-expanded=\"false\"><span class=\"absolute -inset-0.5\"></span> <span class=\"sr-only\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -208,7 +247,7 @@ func AuthorisedLayout() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("--><svg class=\"block h-6 w-6\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" aria-hidden=\"true\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5\"></path></svg><!--")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("--><svg id=\"mobile-menu-closed-icon\" class=\"block h-6 w-6\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" aria-hidden=\"true\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5\"></path></svg><!--")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -217,7 +256,7 @@ func AuthorisedLayout() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("--><svg class=\"hidden h-6 w-6\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" aria-hidden=\"true\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M6 18L18 6M6 6l12 12\"></path></svg></button></div></div></div><!--")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("--><svg id=\"mobile-menu-open-icon\" class=\"hidden h-6 w-6\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" aria-hidden=\"true\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M6 18L18 6M6 6l12 12\"></path></svg></button></div></div></div><!--")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -226,7 +265,7 @@ func AuthorisedLayout() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("--><div class=\"sm:hidden\" id=\"mobile-menu\"><div class=\"space-y-1 pb-3 pt-2\"><!--")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("--><div class=\"hidden\" id=\"mobile-menu\"><div class=\"space-y-1 pb-3 pt-2\"><!--")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
